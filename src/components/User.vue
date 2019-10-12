e18e1168a14adf7bb39ec649457b3be42d3ea297
Jessen Gilbert <jessen.gilbert@yahoo.com>
2019-10-11 22:33:53 -0600

    initial

---
+++ src/components/User.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <p>{{ name }}</p>
+        <p>Age: {{ age }}</p>
+        <button @click="changeName">Change My Name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail 
+                :newName="name" 
+                @nameWasReset="name = $event"
+                :resetFN="resetName"
+                :userAge="age"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit 
+                :userAge="age"
+                @ageWasEdited="age = $event"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data () {
+            return {
+                name: 'Jessen',
+                age: 9000
+            }
+        },
+        methods: {
+            changeName () {
+                this.name = 'Mark'
+            },
+            resetName () {
+                this.name = 'Jessen'
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>{{ newName }}</p>
+        <p>User Name: {{ switchName() }}</p>
+        <p>Age: {{ userAge }}</p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetFN">Reset Name From Parent</button>
+    </div>
+</template>
+
+<script>
+import { eventBus } from '../main.js'
+
+export default {
+    //props: ['newName'],
+    props: {
+        newName: {
+            type: String,
+            //required: true,
+            //default: 'No Name Provided',
+            //Number
+            //Boolean
+            //Function
+            //Object
+            //Array
+        },
+        resetFN: Function,
+        userAge: Number
+    },
+    methods: {
+        switchName () {
+            return this.newName.split('').reverse().join('')
+        },
+        resetName () {
+            this.newName = "Jessen"
+            this.$emit('nameWasReset', this.newName)
+        }
+    },
+    created () {
+        eventBus.$on('ageWasEdited', (age) => { 
+            this.userAge = age;
+        })
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
